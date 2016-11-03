faaedab20dc7089fcbdfd90a22285f377be8ea7f
slobby <andslobby@gmail.com>
2016-11-03 17:25:17 +0300

    fuckulator start

---
+++ Fuckulator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Fuckulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class Fuckulator
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+        public static int Multiply(int x, int y) => x* y;
+        public static int Divide(int x, int y) => x / y;
+    } 
+}
---
+++ Test/UnitTestFuckulator/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Fuckulator;
+
+namespace UnitTestFuckulator
+{
+    [TestFixture]
+    public class UnitTestFuckulator
+    {
+        [Test]
+
+        public void SumOfTwoNumbers()
+
+        {
+
+            Assert.AreEqual(Fuckulator.Fuckulator.Add(5,5), 10);
+
+        }
+    }
+}
